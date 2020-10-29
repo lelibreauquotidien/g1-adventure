970641d9ddc345895f1b943deebc20f50e38c080
Le Libre Au Quotidien <contact@lelibreauquotidien.fr>
2020-10-29 19:35:53 +0100

    add content

---
+++ WaterFountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="WaterFountain" tilewidth="32" tileheight="32" margin="32" tilecount="6" columns="3">
+ <image source="../Téléchargements/WaterFountain.png" width="128" height="96"/>
+</tileset>
---
+++ fleurs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="fleurs" tilewidth="32" tileheight="32" margin="32" tilecount="16" columns="4">
+ <image source="../Téléchargements/tileset.png" width="160" height="160"/>
+</tileset>
